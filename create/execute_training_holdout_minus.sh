ebc3d107a3cc0dfc201d1ddbb8079efd31ba0f53
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-10-10 14:44:23 -0300

    ordenando

--- create/execute_training_holdout_minus.sh
+++ create/execute_training_holdout_minus.sh
@@ -32,22 +32,21 @@ sep=",";
 image_ext=".eps";
 '
 
-#
-BaseDir='/mnt/8811f502-ae19-4dd8-8371-f1915178f581/Fernando'
 # HD
 #BaseDir='/media/fernando/Expansion'
+BaseDir='/mnt/8811f502-ae19-4dd8-8371-f1915178f581/Fernando'
 # 
 #BaseDir='/media/fernando/B0EA304AEA300EDA/Dados/Fernando'
 
-OutDir=$BaseDir'/OUTPUTS/DOCTORADO2/fcnn_emotion4_fusion_1'
+OutDir=$BaseDir'/OUTPUTS/DOCTORADO2/fcnn_emotion4_fusion_2'
 
-DName='ber2024-fusion-minus20'
+DName='ber2024-fusion-step2-minus20'
 
-InTrD=$BaseDir'/DATASET/TESE/BER/BER2024/BER2024-FUSION/ber2024-source/efficientnet_b3_efficientnet_b3_ncod20_minus'
+InTrD=$BaseDir'/DATASET/TESE/BER/BER2024/BER2024-FUSION/ber2024-source/ncod20_efficientnet_b3_efficientnet_b3_step2_minus'
 InTrF='train.csv'
 InDmD=$BaseDir'/DATASET/TESE/BER/BER2024/BER2024-FUSION/dummy/L30000_p0.15'
 InDmF='train.csv'
-InTsD=$BaseDir'/DATASET/TESE/BER/BER2024/BER2024-FUSION/ber2024-source/efficientnet_b3_efficientnet_b3_ncod20_minus'
+InTsD=$BaseDir'/DATASET/TESE/BER/BER2024/BER2024-FUSION/ber2024-source/ncod20_efficientnet_b3_efficientnet_b3_step2_minus'
 InTsF='test.csv'
 
 ################################################################################
@@ -59,8 +58,8 @@ echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/training_va
 
 ipynb-py-convert training_holdout.ipynb training_holdout.py
 
-NcodList=(5 6 7 8 9 10 11 12 13 14 15 16)
-SeedList=(0 0 0 0 0 0  0  0  0  0  0  0)
+NcodList=(6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
+SeedList=(0 0 0 0 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0)
 
 #NcodList=(11 11 11 11 11 11)
 #SeedList=(19 23 29 31 37 41)
